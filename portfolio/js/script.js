5880d8fecf7755bf07e4366275850b758d03d39f
Andriy Khimich <andriykhimi4@gmail.com>
2018-12-03 18:30:02 +0200

    creat me portfolio site

---
+++ portfolio/js/script.js
@@ -0,0 +1,49 @@
+  // preloader
+$(window).on('load', function () {
+  $('.loader_inner').fadeOut();
+  $('.loader').delay(400).fadeOut('slow');
+  $('.header__name').toggleClass('header__name_animation');
+});
+
+$(document).ready(function () {
+
+  // header height
+  function heightDetect() {
+    $('.header').css('min-height', $(window).height());
+  }
+  heightDetect();
+  $(window).resize(function () {
+    heightDetect();
+  });
+  // humburger menu
+  $('.humburger__logo').click(function () {
+    $(this).toggleClass('open');
+    $('.menu').toggleClass('menu_active');
+    $('body').toggleClass('body-fixed');
+    if ($('.menu__container').is(':visible')) {
+      $('.menu__container').fadeOut(600);
+    } else {
+      $('.menu__container').fadeIn(600);
+    }
+  });
+  $('.menu__item').click(function () {
+    $('.menu__container').fadeOut(600);
+    $('.humburger__logo').removeClass('open');
+    $('.menu').toggleClass('menu_active');
+    $('body').toggleClass('body-fixed');
+  });
+  // sloww scroll
+  $('.menu__item a[href^="#"], .footer__item a[href^="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+  });
+
+  // parallax effect
+  $(window).scroll(function () {
+    var scroll = $(this).scrollTop();
+    $('.header__name').css({
+      'transform': 'translate(0%, ' + scroll / 2.5 + '%'
+    });
+  });
+});
